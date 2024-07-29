b9c1625bb84aa6771be15579d943cd7fadf96114
davidhirtz <david.hirtz@me.com>
2024-07-29 17:41:05 +0200

    Version 1.0.1

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+## 1.0.1 (Jul 29, 2024)
+
+- Automatically set Google Maps API key if `googleApiKey` is set in `config/params.php`
--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -4,6 +4,7 @@ namespace davidhirtz\yii2\location\google;
 
 use davidhirtz\yii2\location\google\behaviors\LocationProviderIdBehavior;
 use davidhirtz\yii2\location\google\components\Autocomplete;
+use davidhirtz\yii2\location\google\components\GoogleMapsApi;
 use davidhirtz\yii2\location\models\Location;
 use davidhirtz\yii2\location\modules\admin\widgets\forms\AutocompleteInputWidget;
 use davidhirtz\yii2\skeleton\web\Application;
@@ -20,12 +21,27 @@ class Bootstrap implements BootstrapInterface
     {
         Yii::setAlias('@location-google', __DIR__);
 
-        Event::on(Location::class, Location::EVENT_INIT, function (Event $event) {
-            /** @var Location $location */
-            $location = $event->sender;
-            $location->attachBehavior('LocationProviderIdBehavior', LocationProviderIdBehavior::class);
-        });
+        Event::on(Location::class, Location::EVENT_INIT, $this->attachLocationProviderIdBehavior(...));
 
+        $googleApiKey = $app->params['googleApiKey'] ?? null;
+
+        if ($googleApiKey) {
+            $this->setGoogleApiKey($googleApiKey);
+        }
+
+        $this->setAutocompleteComponent();
+        $this->setAutocompleteInputLabel();
+    }
+
+    protected function attachLocationProviderIdBehavior(Event $event): void
+    {
+        /** @var Location $location */
+        $location = $event->sender;
+        $location->attachBehavior('LocationProviderIdBehavior', LocationProviderIdBehavior::class);
+    }
+
+    protected function setAutocompleteComponent(): void
+    {
         Yii::$app->extendModule('admin', [
             'modules' => [
                 'location' => [
@@ -35,11 +51,21 @@ class Bootstrap implements BootstrapInterface
                 ],
             ],
         ]);
+    }
 
-        if (!Yii::$container->has(AutocompleteInputWidget::class)) {
-            Yii::$container->set(AutocompleteInputWidget::class, [
-                'label' => 'Google Places ID',
-            ]);
-        }
+    protected function setAutocompleteInputLabel(): void
+    {
+        $definition = Yii::$container->getDefinitions()[AutocompleteInputWidget::class] ?? [];
+        $definition['label'] ??= 'Google Places ID';
+
+        Yii::$container->set(AutocompleteInputWidget::class, $definition);
+    }
+
+    protected function setGoogleApiKey(string $googleApiKey): void
+    {
+        $definition = Yii::$container->getDefinitions()[GoogleMapsApi::class] ?? [];
+        $definition['apiKey'] ??= $googleApiKey;
+
+        Yii::$container->set(GoogleMapsApi::class, $definition);
     }
 }
